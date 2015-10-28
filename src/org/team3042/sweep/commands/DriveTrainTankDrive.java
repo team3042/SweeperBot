27c0cc8916877f33ec10054ba5062bcff87addc4
NewUser <NewUser@10.208.194.147>
2015-10-27 20:05:46 -0500

    Fixed Conflict

--- src/org/team3042/sweep/commands/DriveTrainTankDrive.java
+++ src/org/team3042/sweep/commands/DriveTrainTankDrive.java
@@ -23,7 +23,6 @@ public class DriveTrainTankDrive extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        //driveTrain.drive(-oi.stickLeft.getY(), -oi.stickRight.getY());
     }
 
     // Make this return true when this Command no longer needs to run execute()
