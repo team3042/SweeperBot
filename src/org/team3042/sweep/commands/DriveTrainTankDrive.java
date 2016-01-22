49ec47ab638bc119de501aa6fa9af755bfd937e9
NewUser <NewUser@10.208.196.62>
2016-01-21 20:45:14 -0600

    Moved restrictAccel into DriveTrainTankDrive and reset the motor powers within initialize. Added a set motors raw for calibrating in reverse because while foreward was pretty much perfect backwards was a bit off.

--- src/org/team3042/sweep/commands/DriveTrainTankDrive.java
+++ src/org/team3042/sweep/commands/DriveTrainTankDrive.java
@@ -5,6 +5,7 @@
  */
 package org.team3042.sweep.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -15,6 +16,14 @@ public class DriveTrainTankDrive extends CommandBase {
     
     //Scale the joystick values to restrict maximum speed
     private final double speedScale = 1.0;
+    Timer time = new Timer();
+    
+    //Inertial dampening
+    final int LEFT = 0;
+    final int RIGHT = 1;
+    double[] oldTime = new double[] {0, 0};
+    double[] currentPower = new double[] {0,0};
+    double maxAccel = 4.0; //motor power per second
     
     public DriveTrainTankDrive() {
         // Use requires() here to declare subsystem dependencies
@@ -24,12 +33,18 @@ public class DriveTrainTankDrive extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        driveTrain.setMotors(0, 0);
+        driveTrain.resetEncoders();
+        time.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         double leftPower = -oi.stickLeft.getY() * speedScale;
         double rightPower = -oi.stickRight.getY() * speedScale;
+        
+        restrictAccel(leftPower, LEFT);
+        restrictAccel(rightPower, RIGHT);
         
         if(oi.lTrigger.get()){
             rightPower = leftPower;
@@ -55,5 +70,18 @@ public class DriveTrainTankDrive extends CommandBase {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    }
+    
+    private double restrictAccel (double goalValue, int SIDE) {
+        double currentTime = time.get();
+        double dt = currentTime - oldTime[SIDE];
+        oldTime[SIDE] = currentTime;
+        
+        double maxDSpeed = maxAccel * dt;
+        maxDSpeed *= (goalValue >= currentPower[SIDE])? 1 : -1;
+         
+        currentPower[SIDE] = (Math.abs(maxDSpeed) > Math.abs(goalValue - currentPower[SIDE]))? 
+                goalValue : maxDSpeed + currentPower[SIDE];
+        return currentPower[SIDE];
     }
 }
