8c85ce0b58574ad68e872b7e4fb88e0582cb662c
Ethan Lord <ethan.j.lord@gmail.com>
2016-09-27 21:51:35 -0500

    Finished dynamic motion profile generation and added a motion profile for accelerating to a speed to start the path

--- src/org/team3042/sweep/commands/MotionProfileAccelerate.java
+++ src/org/team3042/sweep/commands/MotionProfileAccelerate.java
@@ -1,5 +1,4 @@
 /*
- /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,7 +9,7 @@ package org.team3042.sweep.commands;
  *
  * @author Ethan
  */
-public class DynamicMotionProfileGenerator {
+public class MotionProfileAccelerate {
     //Current point
     int currentPoint = 0;
 
@@ -20,11 +19,8 @@ public class DynamicMotionProfileGenerator {
     //Time for each filter in ms
     double time1, time2;
 
-    //Maximum/minimum speed in rotations / min
-    double maxVelocity, minVelocity;
-
-    //Distance traveled in rotations
-    double distance;
+    //Maximum speed in rotations / min
+    double maxVelocity;
 
     //Length of each filter
     double filterLength1, filterLength2;
@@ -35,9 +31,6 @@ public class DynamicMotionProfileGenerator {
     //Velocity and position at current point
     double currentVelocity = 0, currentPosition = 0, oldVelocity = 0;
 
-    //Time to decceleration in ms
-    double timeToDeccel;
-
     //Total time in ms
     double totalTime;
 
@@ -47,26 +40,17 @@ public class DynamicMotionProfileGenerator {
     //Array with all values of filterSum1
     double[] filterSums1;
 
-    //Generates a standard motion profile that starts and ends at some non-zero speed
-    public DynamicMotionProfileGenerator(int itp, double time1, double time2, double minVelocity,
-                    double maxVelocity, double distance) {
+    public MotionProfileAccelerate(int itp, double time1, double time2, 
+                    double maxVelocity) {
             this.itp = itp;
             this.time1 = time1;
             this.time2 = time2;
-            this.minVelocity = minVelocity;
-            this.maxVelocity = maxVelocity - minVelocity;
-            this.distance = distance;
+            this.maxVelocity = maxVelocity;
 
             //Calculating lengths of two stage filter and number of points
             filterLength1 = Math.ceil(time1 / itp);
             filterLength2 = Math.ceil(time2 / itp);
-            double unscaledTimeToDeccel = distance / maxVelocity * 1000;
-            double unscaledTotalTime = unscaledTimeToDeccel + time1 + time2;
-            double distanceAtMaxVelocity = (unscaledTotalTime - 2 * (time1 + time2)) * maxVelocity;
-            
-            //TODO: Added fudge factor 600 is possibly time1 + time2, untested
-            totalTime = (distanceAtMaxVelocity + 2 * (time1 + time2) * (maxVelocity - minVelocity)) / maxVelocity + 600 * minVelocity / maxVelocity; 
-            timeToDeccel = totalTime - (time1 + time2);
+            totalTime = time1 + time2;
             totalPoints = (int) Math.ceil(totalTime / itp);
             filterSums1 = new double[totalPoints + 1];
     }
@@ -76,13 +60,6 @@ public class DynamicMotionProfileGenerator {
         if (currentPoint * itp < time1) {
             //Accelerating filter 1
             filterSum1 = currentPoint / filterLength1;
-        }
-        else if (currentPoint >= totalPoints - filterLength2) {
-            filterSum1 = 0;
-        }
-        else if (currentPoint * itp >= timeToDeccel) {
-            //Deccelerating filter 1
-            filterSum1 = (totalPoints - filterLength2 - currentPoint) / filterLength1;
         }
         else {
             filterSum1 = 1;
@@ -104,9 +81,7 @@ public class DynamicMotionProfileGenerator {
 
     private void calculateVelocity() {
             oldVelocity = currentVelocity;
-            
-            //Calculates velocity and then adds minimum velocity to ensure non-zero start/end speeds of minVelocity
-            currentVelocity = (filterSum1 + filterSum2) / (filterLength2 + 1) * maxVelocity + minVelocity;
+            currentVelocity = (filterSum1 + filterSum2) / (filterLength2 + 1) * maxVelocity;
     }
 
     public double[][] calculateProfile() {
@@ -122,6 +97,7 @@ public class DynamicMotionProfileGenerator {
                     trajectory[i][0] = currentTime;
                     trajectory[i][1] = currentPosition;
                     trajectory[i][2] = currentVelocity * 60;
+                    System.out.println(currentTime + ": Position: " + currentPosition + ", Velocity: " + currentVelocity);
 
                     currentTime += itp;
                     currentPoint++;
