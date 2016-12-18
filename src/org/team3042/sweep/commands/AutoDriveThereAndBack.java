95d5e6ba9b08936f3e249cef55fe11bd9d6ab46c
NewUser <NewUser@10.208.213.210>
2016-12-18 11:07:45 -0600

    Worked on testing stuff

--- src/org/team3042/sweep/commands/AutoDriveThereAndBack.java
+++ src/org/team3042/sweep/commands/AutoDriveThereAndBack.java
@@ -29,23 +29,28 @@ public class AutoDriveThereAndBack extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-        double courtLength = 8.2;
+        
+        System.out.println("Pass: " + pass);
+        double courtLength = 6;
+        
         if(pass != 1) {
             courtLength -= .3;
         }
+        System.out.println("Length Down: " + courtLength);
         
-        addSequential(new DriveTrainSetGyroGoal(1.5));
+        addSequential(new DriveTrainSetGyroGoal(0.5));
         addSequential(new AutoDrive(courtLength, .3, 1.2, 0, 0)); 
         
         if(pass == 1) {
             courtLength -= .3;
         }
-        
+        System.out.println("Length Back: " + courtLength + "\n");
         // WHEN UPDATING TURN PARAMETERS ALSO UPDATE IN AutoTurnAround! 
         
-        addSequential(new AutoDrive(1.6, .3, 1, 1.4, 1)); 
+        addSequential(new AutoDrive(1.6, .3, 1, 1.4, 1));
+        
         addSequential(new DriveTrainSetGyroGoal(-176));
         addSequential(new AutoDrive(courtLength, .3, 1.2, 0, 0)); 
-                
+              
     }
 }
